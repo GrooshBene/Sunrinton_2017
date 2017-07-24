f086ed00e5d719f5344ec5c596f6538bb5057a2c
GrooshBene <wltn000129@gmail.com>
2017-07-25 00:34:36 +0900

    User Option Update Added

--- routes/send.js
+++ routes/send.js
@@ -1,7 +1,35 @@
 function init(app, User){
+	var mailer = require('nodemailer');
+		function send_mail(reciever, id, password, title, subject, content){
+		var smtpTransport = mailer.createTransport({
+			service : "Gmail",
+
+			auth : {
+				user : id,
+				pass : password
+			}
+		});
+		var mailOptions = {
+			from : title,
+			to : reciever,
+			subject : subject,
+			text : content
+		}
+		smtpTransport.sendMail(mailOptions, function(err, result){
+			if(err){
+				console.log("mail_auth error");
+				throw err;
+			}
+			console.log("Mail Sended : "+ result);
+		});
+	}
 	app.post('/send/kakao', function(req, res){
 	
 	});
+
+	app.post('/send/mail', function(req, res){
+		send_mail(req.param('email'), req.param('id'), req.param('password'), req.param('title'), req.param('subject'), req.param('content'));
+	})
 }
 
 module.exports = init;
--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,26 @@ function init(app, User){
 		});
 	});
 
+	app.post('/user/option/update', function(req, res){
+		User.findOneAndUpdate({_id : req.param('id')}, {options : {
+			feature_lock : req.param('feature_lock'),
+			vibration : req.param('vibration'),
+			flash : req.param('flash'),
+			tts : {
+				value : req.param('tts_value'),
+				text : req.param('tts_text')
+			},
+			alarm : req.param('alarm')
+		}
+		}, {new : true}, function(err, result){
+			if(err){
+				console.log(err);
+				res.send(401, "/user/option/update");
+			}
+			res.send(200, result);
+		});
+	});
+
 
 	app.post('/user/list', function(req, res){
 		User.find({}, function(err, result){
